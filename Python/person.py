da2d2035c04029e526533c0af908f25061f1d25e
Caesar <46439887+Caesar-macharia@users.noreply.github.com>
2024-03-14 22:01:29 +0300

    Add files via upload

---
+++ Python/person.py
@@ -0,0 +1,12 @@
+class Person:
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+
+    def introduce(self):
+            print(f"Hi, my name is {self.name}. I am {self.age} years young and I identify as {self.gender}.")
+
+#Example use
+person1 = Person("Caesar", 15, "male")
+person1.introduce()
